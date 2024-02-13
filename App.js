d1951e444e60f83c12b72ec4a256c39f9b11240c
sankthomas <thomassankara74@gmail.com>
2024-02-13 22:07:22 +0300

    add navigation and fonts

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Leagues from "./screens/Leagues";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+import League from "./screens/League";
+import Team from "./screens/Team";
+import Player from "./screens/Player";
+import Players from "./screens/Players";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    asapRegular: require("./assets/fonts/AsapCondensed-Regular.ttf"),
+    asapBold: require("./assets/fonts/AsapCondensed-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen name="Leagues" component={Leagues} />
+        <Stack.Screen name="League" component={League} />
+        <Stack.Screen name="Team" component={Team} />
+        <Stack.Screen name="Players" component={Players} />
+        <Stack.Screen name="Player" component={Player} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
